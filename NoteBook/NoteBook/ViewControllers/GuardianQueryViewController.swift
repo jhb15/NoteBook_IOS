13218cd19ac0fada1d322f115a687c93eb3b943f
James H Britton <jhb15@aber.ac.uk>
2019-05-04 16:04:15 +0100

    Started work on improvements for app, although it may take time so im gonna leave these out of submission and make it a personal goal.

---
+++ NoteBook/NoteBook/ViewControllers/GuardianQueryViewController.swift
@@ -0,0 +1,46 @@
+//
+//  GuardianQueryViewController.swift
+//  NoteBook
+//
+//  Created by admin on 04/05/2019.
+//  Copyright © 2019 jhb15. All rights reserved.
+//
+
+import UIKit
+
+class GuardianQueryViewController: UIViewController {
+    
+    @IBOutlet weak var queryTextField: UITextField!
+    @IBOutlet weak var addFiltersBtn: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addFiltersAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func executeSearchAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func historyAction(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
